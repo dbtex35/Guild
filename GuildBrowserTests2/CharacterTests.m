7fcd1929be39e9b42fe7bc2082e1685d0e29035f
Darick Barnes <DXB44@rndnug8wl.local>
2016-07-20 11:02:08 -0500

    Added new function

--- GuildBrowserTests2/CharacterTests.m
+++ GuildBrowserTests2/CharacterTests.m
@@ -76,7 +76,9 @@
 -(void)testCreateCharacterFromDetailJsonValidateItems
 {
     Character *_testGuy = [[Character alloc] initWithCharacterDetailData:_characterDetailJson];
-
+    
+    XCTAssertEqualObjects(_testGuy.neckItem.name,@"Stoneheart Choker", @"name is wrong");
+    
     XCTAssertEqualObjects(_testGuy.neckItem.name,@"Stoneheart Choker", @"name is wrong");
     XCTAssertEqualObjects(_testGuy.wristItem.name,@"Vicious Pyrium Bracers", @"name is wrong");
     XCTAssertEqualObjects(_testGuy.waistItem.name,@"Girdle of the Queen's Champion", @"name is wrong");
